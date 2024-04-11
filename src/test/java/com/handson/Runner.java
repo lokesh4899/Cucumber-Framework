195f3e26e31d3a4088c963b30be03f9fa9c15a7f
Datti Lokesh <dattilokesh4@gmail.com>
2024-04-11 13:31:32 +0530

    add json report in runner file

--- src/test/java/com/handson/Runner.java
+++ src/test/java/com/handson/Runner.java
@@ -3,7 +3,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
     
 @CucumberOptions(features = {"src/test/java/Features"}, glue = {"com.stepdefinition"},
-                 plugin = {"html:target/cucumber-reports.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+                 plugin = {"html:target/cucumber-reports.html","json:target/cucumber-reports.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
     
 public class Runner extends AbstractTestNGCucumberTests {
     
